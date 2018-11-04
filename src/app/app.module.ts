a30d8f34c1964b97518596107278a2d74bd73c29
Theo <theosaurus@gmail.com>
2018-11-03 23:09:52 -0500

    Imported Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { ElizaComponent } from './eliza/eliza.component';
-import { ElizabotdataComponent } from './elizabotdata/elizabotdata.component';
 import { ElizabotComponent } from './elizabot/elizabot.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ElizaComponent,
-    ElizabotdataComponent,
     ElizabotComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
