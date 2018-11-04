7e0f2f91f48b805d5d79fac0c4d6598426c1b5a0
Theo <theosaurus@gmail.com>
2018-11-04 13:51:54 -0600

    Added material design and form modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ElizabotComponent } from './elizabot/elizabot.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+import { ChatButtonsComponent } from './chat-buttons/chat-buttons.component';
+import { ChatInterfaceComponent } from './chat-interface/chat-interface.component';
+import { ElizabotComponent } from './elizabot/elizabot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ElizabotComponent
+    ElizabotComponent,
+    ChatButtonsComponent,
+    ChatInterfaceComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/chat-buttons/chat-buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-buttons',
+  templateUrl: './chat-buttons.component.html',
+  styleUrls: ['./chat-buttons.component.css']
+})
+export class ChatButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
